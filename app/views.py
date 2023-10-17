13180d88c22383533c697b5cddaeccaa9f181497
shugokimata <tky2202113@stu.o-hara.ac.jp>
2023-10-17 12:00:10 +0900

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello World. let's start CPB!!これから頑張って行きましょう")
